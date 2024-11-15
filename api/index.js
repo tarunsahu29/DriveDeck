d1e7a20a2dc5a9610cf56549b126ac614db449b9
Tarun Sahu <tarun929s@Taruns-MacBook-Air.local>
2024-11-15 12:04:25 +0530

    deployment issue again fixed

--- api/index.js
+++ api/index.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.static(path.join(__dirname, '../client/dist')))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname,'client','dist','index.html'))
