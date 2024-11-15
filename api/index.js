2e9c80e281f9a47e33e668355f5c804af8a7c520
Tarun Sahu <tarun929s@Taruns-MacBook-Air.local>
2024-11-15 12:10:32 +0530

    please get deployed

--- api/index.js
+++ api/index.js
@@ -46,7 +46,7 @@ app.use('/api/user', userRoutes)
 app.use(express.static(path.join(__dirname, '../client/dist')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+  res.sendFile(path.join(__dirname,'../client','dist','index.html'))
 })
 
 app.listen(PORT, () => {
